416c10fb99851faf48f176e78da0134ad52d2076
Alejandro <alejo8a_2@hotmail.com>
2017-02-25 11:44:59 -0500

    Expectimax naive

---
+++ src/expectimax.py
@@ -0,0 +1,23 @@
+# Cambiar por funcion de evaluación
+
+def value(state):
+  if state.succesors == 0:
+    return state.utility
+
+  if state.isMax:
+    return maxValue(state)
+  else:
+    return expValue(state)
+
+def maxValue(state):
+  value = -inf
+  for child in state.succesors:
+    value = max(v, value(child))
+  return value
+
+def expValue(state):
+  value = 0
+  for child in state.succesors:
+    p = 1/len(state.succesors)
+    value = value + p*value(child)
+  return values
